5a90da2685ead7236376502f6f393b14d6166312
Fabiola Orellana <fabiola.orellana.g@gmail.com>
2019-09-19 18:12:18 -0300

    feature/ Creacion componente Header y Grid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Grid from './components/Grid';
 
-function App() {
+const App = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <Header />
+      <Grid />
+    </React.Fragment>
+  )
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.scss'
+
+const Header = () => {
+	return <header>
+    What the Splash!
+  </header>
+}
+
+export default Header;
