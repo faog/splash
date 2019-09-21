e9d195814afb05632bee660a5cd622a4210c36fb
Fabiola Orellana <fabiola.orellana.g@gmail.com>
2019-09-21 19:31:43 -0300

    fix/ import de statsReducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,11 +1,16 @@
 import { combineReducers } from 'redux';
-import { imagesReducer, loadingReducer, errorReducer, pageReducer } from '../components/ImageGrid/ImageGridRedux/reducers';
+import { imagesReducer, 
+	loadingReducer, 
+	errorReducer, 
+	pageReducer,
+  statsReducer } from '../components/ImageGrid/ImageGridRedux/reducers';
 
 const rootReducer = combineReducers({
 	isLoading: loadingReducer,
 	images: imagesReducer,
 	error: errorReducer,
 	nextPage: pageReducer,
+	imageStats: statsReducer,
 });
 
 export default rootReducer;
